a78f5a52e8b852174b31d448c9294c7f7403e421
ponkiii <rodrihuertaezequiel2005@gmail.com>
2022-11-04 13:07:40 -0300

    rodrijo

--- src/data/productos.js
+++ src/data/productos.js
@@ -5,30 +5,34 @@ import auricular from '../images/auricular.png'
 import maus2 from '../images/maus2.png'
 import mouse from '../images/mouse.jpg'
 import rtx3080 from '../images/rtx3080.png'
+import golden from '../images/golden.png'
+// import ryzen5 from '../images/Rx56.jpg'
+// import gygab from '../images/gygab.png'
+// import amdA from '../images/'
 export const productos = [
   {
-    image: monitor, id: '1', nombre: 'Monitor 60hz', precio: 400, descripcion: 'Monitor Gamer Curvo con una tasa de refrigeracion de 60hz'
+    image: monitor, id: '1', nombre: 'Monitor 60hz', precio: 400, descripcion: ''
   },
   {
-    image: silla, id: '2', nombre: 'Monitor 60hz', precio: 400, descripcion: 'Silla gamer de gran calidad'
+    image: silla, id: '2', nombre: 'Monitor 60hz', precio: 400, descripcion: ''
   },
   {
-    image: computadora, id: '3', nombre: 'Monitor 60hz', precio: 400, descripcion: 'computadora gamer con refrigeracion liquida'
+    image: computadora, id: '3', nombre: 'Monitor 60hz', precio: 400, descripcion: ''
   },
   {
-    image: maus2, id: '4', nombre: 'Mouse gamer ', precio: 400, descripcion: ''
+    image: maus2, id: '4', nombre: 'Mouse gamer Lochitic', precio: 400, descripcion: ''
   },
   {
-    image: auricular, id: '5', nombre: 'Auricular ', precio: 3355, descripcion: 'auricular gamer '
+    image: auricular, id: '5', nombre: 'Auricular ', precio: 3355, descripcion: ''
   },
   {
-    image: mouse, id: '6', nombre: 'mouse logitech', precio: 8000, descripcion: 'mouse logitech '
+    image: mouse, id: '6', nombre: 'mouse logitech', precio: 8000, descripcion: ''
   },
   {
-    image: auricular, id: '7', nombre: 'Monitor 60hz', precio: 400, descripcion: 'Monitor Gamer Curvo con una tasa de refrigeracion de 60hz'
+    image: golden, id: '7', nombre: 'Tarjeta grafica golden edition', precio: 400, descripcion: 'Monitor Gamer Curvo con una tasa de refrigeracion de 60hz'
   },
   {
-    image: auricular, id: '8', nombre: 'Monitor 60hz', precio: 400, descripcion: 'Monitor Gamer Curvo con una tasa de refrigeracion de 60hz'
+    image: rtx3080, id: '8', nombre: 'Nvidia rtx 3080', precio: 400, descripcion: 'Monitor Gamer Curvo con una tasa de refrigeracion de 60hz'
   },
   {
     image: auricular, id: '9', nombre: 'Monitor 60hz', precio: 400, descripcion: 'Monitor Gamer Curvo con una tasa de refrigeracion de 60hz'
--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './Header.css'
-import logo from '../images/rodrigo.png'
+import logo from '../images/amd2.png'
 import { FaShoppingCart, FaSearch } from 'react-icons/fa'
 import { Link } from "react-router-dom";
 
@@ -11,7 +11,7 @@ const Header = () => {
         <img className="logo" src={logo} alt="Logo" />
         <div className="buscador">
           <input placeholder="Buscar" type="text" />
-          <FaSearch className="buscador-icono" fontSize={18} />
+          <FaSearch className="buscador-icono" fontSize={18} color={'#fff'} />
         </div>
         <ul className="container-registro">
           <li className="iniciar-sesion">
